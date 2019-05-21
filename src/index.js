8b800fed7888e15ad215e4ec3f885eb343607749
arailpc <arailpc@protonmail.com>
2019-05-22 00:14:06 +0300

    devserver

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { config } from "./modules/config";
 import "./modules/header.component";
+import "./css/styles.css";
 
 console.log(config);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -7,10 +8,25 @@ module.exports = {
     filename: "bandle.js",
     path: path.resolve(__dirname, "dist")
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    port: 4200
+  },
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css"
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  }
 };
